b6a18cc03fbabe9ef7804373ee8e54fbb9a70335
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-30 18:44:04 +0600

    API edited

--- index.js
+++ index.js
@@ -37,168 +37,33 @@ async function run() {
        res.send(tasks)
      })
  
+     app.post('/tasks', async (req, res) => {
+       const task = req.body
+       const result = await tasksCollection.insertOne(task)
+       res.send(result)
+     })
+
      app.get('/tasks/:id', async (req, res) => {
-       const id = req.params.id
-       const query = { _id: ObjectId(id) }
-       const task = await tasksCollection.findOne(query)
-       res.send(task)
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const task = await tasksCollection.findOne(query)
+      res.send(task)
      })
  
-     app.get('/bookings', verifyJWT, async (req, res) => {
-       const email = req.query.email
-       const decodedEmail = req.decoded.email
- 
-       if (email !== decodedEmail) {
-         return res
-           .status(403)
-           .send({ message: 'forbidden access', decodedEmail })
-       }
- 
-       const query = { email: email }
-       const cursor = bookingsCollection.find(query)
-       const bookings = await cursor.toArray()
-       res.send(bookings)
-     })
- 
-     app.post('/bookings', async (req, res) => {
-       const booking = req.body
- 
-       const query = {
-         email: booking.email,
-         product: booking.product,
-       }
- 
-       const alreadyBooked = await bookingsCollection.find(query).toArray()
- 
-       if (alreadyBooked.length) {
-         const message = `You have already booked ${booking.product}`
-         return res.send({ acknowledged: false, message })
-       }
- 
-       const result = await bookingsCollection.insertOne(booking)
-       res.send(result)
-     })
- 
-     app.get('/bookings/:id', async (req, res) => {
-       const id = req.params.id
-       const query = { _id: ObjectId(id) }
-       const booking = await bookingsCollection.findOne(query)
-       res.send(booking)
-     })
- 
-     app.get('/wishlists', async (req, res) => {
-       const email = req.query.email
-       const query = { email: email }
-       const cursor = wishlistsCollection.find(query)
-       const wishlists = await cursor.toArray()
-       res.send(wishlists)
-     })
- 
-     app.post('/wishlists', async (req, res) => {
-       const wishlist = req.body
-       const query = {
-         email: wishlist.email,
-         product: wishlist.product,
-       }
-       const alreadyAdded = await wishlistsCollection.find(query).toArray()
-       if (alreadyAdded.length) {
-         const message = `You have already added ${wishlist.product} to your wishlist`
-         return res.send({ acknowledged: false, message })
-       }
-       const result = await wishlistsCollection.insertOne(wishlist)
-       res.send(result)
-     })
- 
-     app.post('/create-payment-intent', async (req, res) => {
-       const booking = req.body
-       const price = booking.price
-       const amount = price * 100
- 
-       const paymentIntent = await stripe.paymentIntents.create({
-         currency: 'bdt',
-         amount: amount,
-         payment_method_types: ['card'],
-       })
-       res.send({
-         clientSecret: paymentIntent.client_secret,
-       })
-     })
- 
-     // payment API
-     app.post('/payments', async (req, res) => {
-       const payment = req.body
-       const result = await paymentsCollection.insertOne(payment)
-       const id = payment.bookingId
-       const filter = { _id: ObjectId(id) }
-       const updatedDoc = {
-         $set: {
-           paid: true,
-           transactionId: payment.transactionId,
-         },
-       }
-       const updatedResult = await bookingsCollection.updateOne(
-         filter,
-         updatedDoc
-       )
-       res.send(result)
-     })
- 
-     // Token API
-     app.get('/jwt', async (req, res) => {
-       const email = req.query.email
-       const query = { email: email }
-       const user = await usersCollection.findOne(query)
-       console.log(user)
-       if (user) {
-         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
-           expiresIn: '1d',
-         })
-         return res.send({ accessToken: token })
-       }
-       res.status(403).send({ accessToken: '' })
-     })
- 
-     app.get('/users', async (req, res) => {
-       const query = {}
-       const users = await usersCollection.find(query).toArray()
-       res.send(users)
-     })
- 
-     // save users in database
-     app.post('/users', async (req, res) => {
-       const user = req.body
-       console.log(user)
-       const result = await usersCollection.insertOne(user)
-       res.send(result)
-     })
- 
-     app.get('/users/admin/:email', async (req, res) => {
-       const email = req.params.email
-       const query = { email }
-       const user = await usersCollection.findOne(query)
-       res.send({ isAdmin: user?.role === 'admin' })
-     })
- 
-     // make admin
-     app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
-       const decodedEmail = req.decoded.email
-       const query = { email: decodedEmail }
-       console.log(query)
-       const user = await usersCollection.findOne(query)
- 
-       if (user?.role !== 'admin') {
-         return res.status(403).send({ message: 'forbidden access' })
-       }
- 
+     app.put('/tasks/:id' async (req, res) => {
        const id = req.params.id
        const filter = { _id: ObjectId(id) }
+ 
        const options = { upsert: true }
        const updatedDoc = {
          $set: {
-           role: 'admin',
+           completed: {
+            // toggles the value of completed key using bitwise operation
+            $bit: { and: [1, { $not: "$isActive" }] }
+          }
          },
        }
-       const result = await usersCollection.updateOne(
+       const result = await tasksCollection.updateOne(
          filter,
          updatedDoc,
          options
@@ -206,67 +71,10 @@ async function run() {
        res.send(result)
      })
  
-     // verify seller
-     app.put('/users/sellers/:id', verifyJWT, verifyAdmin, async (req, res) => {
-       const decodedEmail = req.decoded.email
-       const query = { email: decodedEmail }
-       console.log(query)
-       const user = await usersCollection.findOne(query)
- 
-       if (user?.role !== 'admin') {
-         return res.status(403).send({ message: 'forbidden access' })
-       }
- 
+     app.delete('/tasks/:id', async (req, res) => {
        const id = req.params.id
        const filter = { _id: ObjectId(id) }
-       const options = { upsert: true }
-       const updatedDoc = {
-         $set: {
-           verified: true,
-         },
-       }
-       const result = await usersCollection.updateOne(
-         filter,
-         updatedDoc,
-         options
-       )
-       res.send(result)
-     })
- 
-     app.get('/products', verifyJWT, verifyAdmin, async (req, res) => {
-       const query = {}
-       const products = await productsCollection.find(query).toArray()
-       res.send(products)
-     })
- 
-     app.post('/products', verifyJWT, verifyAdmin, async (req, res) => {
-       const product = req.body
-       const result = await productsCollection.insertOne(product)
-       res.send(result)
-     })
- 
-     app.put('/products/:id', verifyJWT, verifyAdmin, async (req, res) => {
-       const id = req.params.id
-       const filter = { _id: ObjectId(id) }
- 
-       const options = { upsert: true }
-       const updatedDoc = {
-         $set: {
-           advertised: true,
-         },
-       }
-       const result = await productsCollection.updateOne(
-         filter,
-         updatedDoc,
-         options
-       )
-       res.send(result)
-     })
- 
-     app.delete('/products/:id', verifyJWT, verifyAdmin, async (req, res) => {
-       const id = req.params.id
-       const filter = { _id: ObjectId(id) }
-       const result = await productsCollection.deleteOne(filter)
+       const result = await tasksCollection.deleteOne(filter)
        res.send(result)
      })
    } finally {
@@ -276,16 +84,6 @@ async function run() {
 
 
 // ------------------
-
-// const users = [
-//   { id: 1, name: 'Masha', email: 'mashaislam@gmail.com' },
-//   { id: 2, name: 'Minar', email: 'minarrahman@gmail.com' },
-//   { id: 3, name: 'Pritom', email: 'pritomhasan@gmail.com' },
-// ]
-
-// app.get('/users', (req, res) => {
-//   res.send(users)
-// })
 
 app.get('/', async (req, res) => {
    res.send('Task Master Node Server is running')
